8ababc8610c5fe9347ae58f0d94f7e85b4bad6f7
ghostie <y_jw@outlook.com>
2018-07-25 11:22:07 +0800

    参考更好的解法， 改用模式匹配和递归来解决问题

--- protein-translation/protein_translation.exs
+++ protein-translation/protein_translation.exs
@@ -4,65 +4,48 @@ defmodule ProteinTranslation do
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
-    to_charlist(rna)
-    |> Enum.chunk_every(3)
-    |> Enum.map(fn c -> of_codon(to_string(c)) end)
-    |> Enum.reduce([], fn
-      (_, {:ok, res}) -> {:ok, res}
-      (_, {:error, res}) -> {:error, res}
-      ({:ok, "STOP"}, acc) -> {:ok, acc}
-      ({:ok, rna}, acc) -> acc ++ [rna]
-      ({:error, _}, _) -> {:error, "invalid RNA"}
-    end)
-    |> (fn
-      {:ok, res} -> {:ok, res}
-      {:error, res} -> {:error, res}
-      res -> {:ok, res}
-    end).()
+    do_of_rna(rna, [])
+  end
+
+  def do_of_rna("", proteins), do: {:ok, proteins}
+
+  def do_of_rna(<<codon::binary-size(3), rest::binary>>, proteins) do
+    case of_codon(codon) do
+      {:error, _} -> {:error, "invalid RNA"}
+      {:ok, "STOP"} -> {:ok, proteins}
+      {:ok, protein} -> do_of_rna(rest, proteins ++ [protein])
+    end
   end
 
   @doc """
   Given a codon, return the corresponding protein
-
-  UGU -> Cysteine
-  UGC -> Cysteine
-  UUA -> Leucine
-  UUG -> Leucine
-  AUG -> Methionine
-  UUU -> Phenylalanine
-  UUC -> Phenylalanine
-  UCU -> Serine
-  UCC -> Serine
-  UCA -> Serine
-  UCG -> Serine
-  UGG -> Tryptophan
-  UAU -> Tyrosine
-  UAC -> Tyrosine
-  UAA -> STOP
-  UAG -> STOP
-  UGA -> STOP
   """
+
+  @codon2Proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
-    case codon do
-      "UGU" -> {:ok, "Cysteine"}
-      "UGC" -> {:ok, "Cysteine"}
-      "UUA" -> {:ok, "Leucine"}
-      "UUG" -> {:ok, "Leucine"}
-      "AUG" -> {:ok, "Methionine"}
-      "UUU" -> {:ok, "Phenylalanine"}
-      "UUC" -> {:ok, "Phenylalanine"}
-      "UCU" -> {:ok, "Serine"}
-      "UCC" -> {:ok, "Serine"}
-      "UCA" -> {:ok, "Serine"}
-      "UCG" -> {:ok, "Serine"}
-      "UGG" -> {:ok, "Tryptophan"}
-      "UAU" -> {:ok, "Tyrosine"}
-      "UAC" -> {:ok, "Tyrosine"}
-      "UAA" -> {:ok, "STOP"}
-      "UAG" -> {:ok, "STOP"}
-      "UGA" -> {:ok, "STOP"}
-      _ -> {:error, "invalid codon"}
+    case Map.fetch(@codon2Proteins, codon) do
+      :error -> {:error, "invalid codon"}
+      res -> res
     end
   end
 end
