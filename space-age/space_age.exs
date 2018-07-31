29777f8367e18b6551ffce9b2e42a9ed91143c18
ghostie <y_jw@outlook.com>
2018-07-31 15:33:54 +0800

    add space age

---
+++ space-age/space_age.exs
@@ -0,0 +1,38 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @age_maps %{
+    :mercury => 0.2408467,
+    :venus => 0.61519726,
+    :mars => 1.8808158,
+    :jupiter => 11.862615,
+    :saturn => 29.447498,
+    :uranus => 84.016846,
+    :neptune => 164.79132
+  }
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    case planet do
+      :earth -> earth_age(seconds)
+      _ -> earth_age(seconds) / Map.get(@age_maps, planet)
+    end
+  end
+
+  @earth_year 365.25
+  defp earth_age(seconds) do
+    seconds / @earth_year / 24 / 3600
+  end
+end
