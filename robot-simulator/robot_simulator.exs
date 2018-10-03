15ef5590aa869b07e109996b828cc7734482c55e
ghostie <y_jw@outlook.com>
2018-10-04 07:37:48 +0800

    update pattern matching

--- robot-simulator/robot_simulator.exs
+++ robot-simulator/robot_simulator.exs
@@ -8,29 +8,17 @@ defmodule RobotSimulator do
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
   def create(direction \\ :north, position \\ {0,0}) do
-    with :ok <- is_valid_direction(direction),
-      :ok <- is_valid_position(position) do
+    with :ok <- validate_direction(direction),
+      :ok <- validate_position(position) do
         %RobotSimulator{direction: direction, position: position}
-    else
-      {:error, reason} -> {:error, reason}
     end
   end
 
-  defp is_valid_direction(direction) do
-    case direction in [:north,:east,:south,:west] do
-      true -> :ok
-      false -> {:error, "invalid direction"}
-    end
-  end
+  defp validate_direction(direction) when direction in [:north,:east,:south,:west], do: :ok
+  defp validate_direction(_), do: {:error, "invalid direction"}
 
-  defp is_valid_position(position) do
-    with {x,y} <- position,
-      true <- is_integer(x) and is_integer(y) do
-        :ok
-    else
-      _ -> {:error, "invalid position"}
-    end 
-  end
+  defp validate_position({x,y}) when is_integer(x) and is_integer(y), do: :ok
+  defp validate_position(_), do: {:error, "invalid position"}
 
   @doc """
   Simulate the robot's movement given a string of instructions.
@@ -39,20 +27,12 @@ defmodule RobotSimulator do
   """
   @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, instructions) do
-    case String.match?(instructions, ~r/^[RLA]+$/) do
-      false -> {:error, "invalid instruction"}
-      true -> String.split(instructions, "", trim: true)
-        |> Enum.reduce(robot, fn (instruction, robot) -> 
-          case instruction do
-            "A" -> move_position(robot)
-            direction -> turn_direction(robot, direction) 
-          end
-        end) 
-    end
+    instructions
+    |> String.graphemes
+    |> Enum.reduce(robot, &move(&1, &2))
   end
 
-  defp move_position(robot) do
-    %{direction: direction} = robot
+  defp move("A", %{direction: direction} = robot) do
     Map.update!(robot, :position, fn {x,y} ->
       case direction do
         :north -> {x, y + 1} 
@@ -65,14 +45,17 @@ defmodule RobotSimulator do
 
   @clockwise [north: :east, east: :south, south: :west, west: :north]
   @anticlockwise [north: :west, east: :north, south: :east, west: :south]
-  defp turn_direction(robot, direction_action) do
-    Map.update!(robot, :direction, fn direction -> 
-      case direction_action do
-        "R" -> Keyword.get(@clockwise, direction)
-        "L" -> Keyword.get(@anticlockwise, direction)
-      end
+  defp move("R", %{direction: direction} = robot) do
+    Map.update!(robot, :direction, fn _ -> 
+        Keyword.get(@clockwise, direction)
     end)
   end
+  defp move("L", %{direction: direction} = robot) do
+    Map.update!(robot, :direction, fn _ -> 
+        Keyword.get(@anticlockwise, direction)
+    end)
+  end
+  defp move(_, _), do: {:error, "invalid instruction"}
 
   @doc """
   Return the robot's direction.
@@ -80,11 +63,11 @@ defmodule RobotSimulator do
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec direction(robot :: any) :: atom
-  def direction(robot), do: Map.get(robot, :direction)
+  def direction(robot), do: robot.direction
 
   @doc """
   Return the robot's position.
   """
   @spec position(robot :: any) :: {integer, integer}
-  def position(robot), do: Map.get(robot, :position)
+  def position(robot), do: robot.position
 end
